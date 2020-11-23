3a0c092a8ace8a29c3e1278c354e319ff8d2ae31
nobregapmo@esffranco.edu.pt <nobregapmo@esffranco.edu.pt>
2020-11-23 01:01:57 +0000

    Add project files.

---
+++ App6/App6/PaginaDetalhes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App6
+{
+    public class PaginaDetalhes : ContentPage
+    {
+        public PaginaDetalhes(Jogo oJogo)
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = oJogo.Titulo }
+                }
+            };
+        }
+    }
+}
