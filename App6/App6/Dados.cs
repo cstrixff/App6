e1b367e6b794e6f51abf4a9e07c232edf9436f16
nobregapmo@esffranco.edu.pt <nobregapmo@esffranco.edu.pt>
2020-11-23 14:48:28 +0000

    limpar Dados.cs

--- App6/App6/Dados.cs
+++ App6/App6/Dados.cs
@@ -17,18 +17,17 @@ namespace App6
   
         static private void AbrirLigacao()
         {
-           // try
-           // {
+            try
+            {
                 conn = new MySqlConnection(conString);
                 conn.Open();
-               // Toast.MakeText(Application.Context, "Ligação estabelecida com sucesso", ToastLength.Long).Show();
                
-           // }
-           // catch (Exception ex)
-           // {
+            }
+            catch (Exception ex)
+            {
 
-               // Toast.MakeText(Application.Context, "Erro na ligação" + ex, ToastLength.Long).Show();
-            //}
+             
+            }
         }
 
         static public List<Jogo> Jogos()
@@ -44,12 +43,9 @@ namespace App6
             
             leitor = cmd.ExecuteReader();
             
-           // MessageBox.Show(leitor.FieldCount + " z"+ leitor.HasRows.ToString() + " "+cmd.ToString());
             while (leitor.Read())
             {
-                //MessageBox.Show("as");
                 Jogo jogo = new Jogo(leitor["ID"].ToString(), leitor["titulo"].ToString(), leitor["preco"].ToString(), leitor["capa"].ToString());
-                //Jogo jogo = new Jogo(12, "12", 12);
 
                 listaJogos.Add(jogo);
 
@@ -69,39 +65,7 @@ namespace App6
             }
             catch (Exception ex)
             {
-
-                //Toast.MakeText(Application.Context, "Erro na ligação" + ex, ToastLength.Long).Show();
             }
         }
-        /*
-        static public Utilizador Login(string username, string password)
-        {
-            AbrirLigacao();
-            Utilizador utilizador;
-
-            var querysql = "SELECT * FROM utilizador WHERE username=@user AND password = @pass";
-
-            MySqlDataReader leitor = null;
-            MySqlCommand cmd = new MySqlCommand(querysql, conn);
-            cmd.Parameters.AddWithValue("@user", username);
-            cmd.Parameters.AddWithValue("@pass", password);
-            leitor = cmd.ExecuteReader();
-
-            //MessageBox.Show(leitor.FieldCount + " z"+ leitor.HasRows.ToString() + " "+cmd.ToString());
-            if (leitor.HasRows)
-            {
-                leitor.Read();
-                utilizador = new Utilizador(Convert.ToInt32(leitor["ID"]), leitor["nome"].ToString(), leitor["username"].ToString(), leitor["password"].ToString(), Convert.ToInt32(leitor["tipo"]));
-                FecharLigacao();
-                return utilizador;
-                                  
-            }
-            FecharLigacao();
-            return null;
-
-
-
-
-        }*/
     }
 }
